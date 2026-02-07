ec7f1257d03ec69df17d5e64b2085d0ab718038b
MaxCherya <maxchergik@gmail.com>
2026-02-07 15:56:14 +0000

    feat(client): :sparkles: Describe main features on landing page
    Create a component which describes main features and modules of the product

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/landing/Hero";
 import SmallDetails from "@/components/landing/SmallDetails";
+import WhyItsGood from "@/components/landing/WhyItsGood";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <SmallDetails />
+      <WhyItsGood />
     </main>
   );
 }
